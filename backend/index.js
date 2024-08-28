88a06c1196bd2247ea3c7bca16caf071cee50f03
Damian <www.damianchamel@gmail.com>
2024-08-28 12:30:48 +0530

    Simple get request

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("QuizzyPals backend is up and running at: http://localhost:4000...");
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}...`);
 });
