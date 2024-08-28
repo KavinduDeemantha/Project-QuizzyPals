8f981bc6b1d2094a76877a4497281c7918879a3f
Damian <www.damianchamel@gmail.com>
2024-08-28 12:31:38 +0530

    Added cloud based mongodb connection to the back end.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
 
@@ -9,6 +10,13 @@ app.get("/", (req, res) => {
     res.send("QuizzyPals backend is up and running at: http://localhost:4000...");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}...`);
-});
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+mongoClient.connect()
+            .then(() => {
+                app.listen(process.env.PORT, () => {
+                    console.log(`Listening on port ${process.env.PORT}...`);
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
