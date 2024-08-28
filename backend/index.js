9f443ea056c2f630d72417a796d26fdf58b842a3
Damian <www.damianchamel@gmail.com>
2024-08-28 11:49:58 +0530

    Set up Express server with dotenv configuration

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
 
 const app = express();
 
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}...`);
+});
