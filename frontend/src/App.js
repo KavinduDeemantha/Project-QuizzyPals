3457edf1f720d466dc8802569596f43760c97f24
Damian <www.damianchamel@gmail.com>
2024-09-19 00:30:00 +0530

    added room lobby page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WelcomePage from './pages/WelcomePage';
 import SignInPage from './pages/SignInPage';
+import RoomLobbyPage from './pages/RoomLobbyPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/welcomepage' element={<WelcomePage />} />
           <Route path='/signin' element={<SignInPage />} />
+          <Route path='/lobby' element={<RoomLobbyPage />} />
         </Routes>
       </BrowserRouter>
     </div>
