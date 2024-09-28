0ea4767b18c4f58ab3b97cb84517e8c1557bf1d2
Damian <www.damianchamel@gmail.com>
2024-09-28 23:56:53 +0530

    Added game quiz creation page / question page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WelcomePage from './pages/WelcomePage';
 import SignInPage from './pages/SignInPage';
 import RoomLobbyPage from './pages/RoomLobbyPage';
+import GameQuestionRound from './pages/GameQuestionRound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/welcomepage' element={<WelcomePage />} />
           <Route path='/signin' element={<SignInPage />} />
           <Route path='/roomlobby' element={<RoomLobbyPage />} />
+          <Route path='/question' element={<GameQuestionRound />} />
         </Routes>
       </BrowserRouter>
     </div>
