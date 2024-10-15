44e7c284e668a785699a215d5899a76abcca6627
Kavindu Deemantha <kavindudeemantha24@gmail.com>
2024-10-16 00:26:57 +0530

    set sign in page as the landing page (#10) (#11)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import WelcomePage from './pages/WelcomePage';
-import SignInPage from './pages/SignInPage';
-import RoomLobbyPage from './pages/RoomLobbyPage';
-import GameQuestionRound from './pages/GameQuestionRound';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
+import SignInPage from "./pages/SignInPage";
+import RoomLobbyPage from "./pages/RoomLobbyPage";
+import GameQuestionRound from "./pages/GameQuestionRound";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/welcomepage' element={<WelcomePage />} />
-          <Route path='/signin' element={<SignInPage />} />
-          <Route path='/roomlobby' element={<RoomLobbyPage />} />
-          <Route path='/createquiz' element={<GameQuestionRound />} />
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/welcomepage" element={<WelcomePage />} />
+          <Route path="/roomlobby" element={<RoomLobbyPage />} />
+          <Route path="/createquiz" element={<GameQuestionRound />} />
         </Routes>
       </BrowserRouter>
     </div>
