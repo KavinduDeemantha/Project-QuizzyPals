d8c1e9e15fbfa9c45d33de566696332a741fa4c2
Kavindu Deemantha <kavindudeemantha24@gmail.com>
2024-10-29 01:40:50 +0530

    leaderboard UI done

--- frontend/src/pages/LeaderboardPage.js
+++ frontend/src/pages/LeaderboardPage.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import Grid from "@mui/material/Grid2";
 
-import "./RoomLobbyPage.css";
+import "./LeaderboardPage.css";
 import FormInputComponent from "../components/FormInputComponent";
 import ButtonComponent from "../components/ButtonComponent";
 import { List, ListItem, ListItemText } from "@mui/material";
@@ -30,6 +30,8 @@ const rows = [
   createData("Player 7", 40),
   createData("Player 8", 100),
   createData("Player 9", 100),
+  createData("Player 9", 100),
+  createData("Player 10", 100)
 ];
 
 const LeaderboardPage = () => {
@@ -43,39 +45,23 @@ const LeaderboardPage = () => {
   return (
     <Grid container columns={16}>
       <Grid size={8}>
-        <div
-          style={{
-            borderRight: "1px solid black",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <div className="bigname">QuizzyPals</div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              gap: 10,
-              marginTop: 190,
-            }}
-          >
+        <div className="header-container">
+          <div className="header">QuizzyPals</div>
+          <div className="three-btn-container ">
             <div className="button-container">
-              <ButtonComponent
+              <ButtonComponent className="leaderboard-btns"
                 label={"Start new game"}
                 onClick={() => navigate("/createquiz")}
               />
             </div>
             <div>
-              <ButtonComponent
+              <ButtonComponent className="leaderboard-btns"
                 label={"Go to lobby"}
                 onClick={() => navigate("/roomlobby")}
               />
             </div>
             <div>
-              <ButtonComponent
+              <ButtonComponent className="leaderboard-btns"
                 label={"Exit to home"}
                 onClick={() => navigate("/welcomepage")}
               />
@@ -84,21 +70,14 @@ const LeaderboardPage = () => {
         </div>
       </Grid>
       <Grid size={8}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyItems: "center",
-          }}
-        >
-          <div className="pagetitle">LEADERBOARD</div>
-          <div className="subtitle">RoomCode</div>
+        <div className="page-title-container">
+          <div className="page-title">LEADERBOARD</div>
+          <div className="sub-title">RoomCode</div>
 
           <div className="player-list-box-outer">
-            <div style={{ fontSize: 24, marginBottom: 10 }}>Players</div>
+            <div className="players">Players</div>
             <div className="player-list-box-inner">
-              <table className="player-score-table" border="1">
+              <table className="player-score-table">
                 <tr>
                   <th>Player Name</th>
                   <th>Score</th>
