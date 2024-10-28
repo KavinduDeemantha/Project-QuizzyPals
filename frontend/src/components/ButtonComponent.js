c8c4563d143cacb1c8d556e29b1e6729b85ae22f
Kavindu Deemantha <kavindudeemantha24@gmail.com>
2024-10-29 01:52:50 +0530

    leaderboard UI done (#21) (#22)

--- frontend/src/components/ButtonComponent.js
+++ frontend/src/components/ButtonComponent.js
@@ -1,11 +1,11 @@
 import { Button } from "@mui/material";
 import "../App.css";
 
-const ButtonComponent = ({ label, onClick, isDisabled = false }) => {
+const ButtonComponent = ({ label, onClick, isDisabled = false, className }) => {
   return (
     <>
       <Button
-        className="btn-component"
+        className={`btn-component ${className}`}
         disabled={isDisabled}
         onClick={onClick}
         variant="contained"
