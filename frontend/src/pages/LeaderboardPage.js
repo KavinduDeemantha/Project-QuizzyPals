ebcdbfa8b2a95b17c8e1c967509fcec94e376271
Damian <www.damianchamel@gmail.com>
2024-11-27 17:37:20 +0530

    Integrated back-end functionality with the leader board page

--- frontend/src/pages/LeaderboardPage.js
+++ frontend/src/pages/LeaderboardPage.js
@@ -15,6 +15,11 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { useRoomContext } from "../hook/useRoomContext";
+import { useEffect } from "react";
+import axios from "axios";
+import { useAuthContext } from "../hook/useAuthContext";
+import { useGameContext } from "../hook/useGameContext";
 
 function createData(name, score) {
   return { name, score };
@@ -36,11 +41,49 @@ const rows = [
 
 const LeaderboardPage = () => {
   const navigate = useNavigate();
+  const { room } = useRoomContext();
+  const { user } = useAuthContext();
   const [playersInRoom, setPlayersInRoom] = useState([
     "Player 1",
     "Player 2",
     "Player 3",
   ]);
+
+  const requestHeaders = {
+    headers: {
+      Authorization: `Bearer ${user.userJWT}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const getAndSetRoomPlayers = async (roomId) => {
+    await axios
+      .get(
+        `${process.env.REACT_APP_BASE_URL}/api/rooms/getroommates/${roomId}`,
+        requestHeaders
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          const players = [];
+          for (let player of response.data) {
+            players.push({ name: player.email, score: 0 });
+          }
+
+          setPlayersInRoom(players);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    if (room) {
+      getAndSetRoomPlayers(room.roomId);
+    } else {
+      console.error("Room destroyed");
+    }
+  }, []);
 
   return (
     <Grid container columns={16}>
@@ -75,24 +118,28 @@ const LeaderboardPage = () => {
       <Grid size={8}>
         <div className="page-title-container">
           <div className="page-title">LEADERBOARD</div>
-          <div className="sub-title">RoomCode</div>
+          <div className="sub-title">RoomCode: {room && room.roomId}</div>
 
           <div className="player-list-box-outer">
             <div className="players">Players</div>
             <div className="player-list-box-inner">
               <table className="player-score-table">
-                <tr>
-                  <th>Player Name</th>
-                  <th>Score</th>
-                </tr>
-                {rows.map((item) => {
-                  return (
-                    <tr>
-                      <td>{item.name}</td>
-                      <td>{item.score}</td>
-                    </tr>
-                  );
-                })}
+                <thead>
+                  <tr>
+                    <th>Player Name</th>
+                    <th>Score</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {playersInRoom.map((item, i) => {
+                    return (
+                      <tr key={i}>
+                        <td>{item.name}</td>
+                        <td>{item.score}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
               </table>
             </div>
           </div>
